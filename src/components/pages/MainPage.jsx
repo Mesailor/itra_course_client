efed3f0197d2e81f5454709972c9bda0a6e99e21
MeSailor <themicroline@gmail.com>
2024-02-28 21:10:26 +0400

    user can logout

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,11 +1,22 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import { setUser } from "../../store/userSlice";
+import { useDispatch } from "react-redux";
 
 export default function MainPage() {
+  const dispatch = useDispatch();
+
+  function logout() {
+    dispatch(setUser({ name: "", isAdmin: false }));
+  }
+
   return (
     <div className="main-page">
       <header>
         <Link to="/log-in">Log in</Link>
+        <Link onClick={logout} to="/log-in">
+          Log out
+        </Link>
       </header>
       <main>MAIN PAGE</main>
       <p>MAIN PAGE TEXT</p>
