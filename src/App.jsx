1588cf6a4d9425b62f8a10034b5e985764392581
MeSailor <themicroline@gmail.com>
2024-02-28 11:29:54 +0400

    add error handling page

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,23 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainPage from "./components/pages/MainPage";
 import LoginPage from "./components/pages/LoginPage";
 import SignupPage from "./components/pages/SignupPage";
+import ErrorPage from "./components/pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainPage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/log-in",
     element: <LoginPage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/sign-up",
     element: <SignupPage />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
---
+++ src/components/pages/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import { Link, useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.log(error);
+
+  return (
+    <div className="error-page">
+      <h2>Oops... It seems like something went wrong :(</h2>
+      <p>Status: {error.status}</p>
+      <p>{error.data}</p>
+      <Link to="/">Back to the main page</Link>
+    </div>
+  );
+}
