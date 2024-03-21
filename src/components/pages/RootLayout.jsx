b5dc7270da75fd23bda0375434fa90aef9e1750c
MeSailor <themicroline@gmail.com>
2024-03-21 20:11:51 +0400

    icons should be i elements

--- src/components/pages/RootLayout.jsx
+++ src/components/pages/RootLayout.jsx
@@ -80,19 +80,7 @@ export default function RootLayout() {
                     aria-expanded="false"
                     aria-controls="collapseAccount"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-list"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-                      />
-                    </svg>
+                    <i className="bi bi-list"></i>
                   </a>
                 </>
               ) : (
@@ -111,19 +99,7 @@ export default function RootLayout() {
                     aria-expanded="false"
                     aria-controls="collapseAccount"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-list"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
-                      />
-                    </svg>
+                    <i className="bi bi-list"></i>
                   </a>
                 </>
               )}
