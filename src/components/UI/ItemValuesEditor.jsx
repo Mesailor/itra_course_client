9c9ffe994b0e3b132db41b1ee7a63c1e9b0c509d
MeSailor <themicroline@gmail.com>
2024-03-27 10:47:33 +0400

    styles for item modals

--- src/components/UI/ItemValuesEditor.jsx
+++ src/components/UI/ItemValuesEditor.jsx
@@ -11,245 +11,290 @@ export default function ItemValuesEditor({
 
   return (
     <>
-      <div
-        className="field-input"
+      <li
+        className="list-group-item"
         style={{
           display: !itemsSchema.custom_str1_name && "none",
         }}
       >
-        <label>{itemsSchema.custom_str1_name}</label>
+        <label className="form-label fw-bold">
+          {itemsSchema.custom_str1_name}
+        </label>
         <input
+          className="form-control"
           onChange={(e) => {
             updateFields("str1", e.target.value);
           }}
           type="text"
           value={itemFieldsValues.custom_str1_value}
         />
-      </div>
+      </li>
 
-      <div
-        className="field-input"
+      <li
+        className="list-group-item"
         style={{
           display: !itemsSchema.custom_str2_name && "none",
         }}
       >
-        <label>{itemsSchema.custom_str2_name}</label>
+        <label className="form-label fw-bold">
+          {itemsSchema.custom_str2_name}
+        </label>
         <input
+          className="form-control"
           onChange={(e) => {
             updateFields("str2", e.target.value);
           }}
           type="text"
           value={itemFieldsValues.custom_str2_value}
         />
-      </div>
+      </li>
 
-      <div
-        className="field-input"
+      <li
+        className="list-group-item"
         style={{
           display: !itemsSchema.custom_str3_name && "none",
         }}
       >
-        <label>{itemsSchema.custom_str3_name}</label>
+        <label className="form-label fw-bold">
+          {itemsSchema.custom_str3_name}
+        </label>
         <input
+          className="form-control"
           onChange={(e) => {
             updateFields("str3", e.target.value);
           }}
           type="text"
           value={itemFieldsValues.custom_str3_value}
         />
-      </div>
+      </li>
 
-      <div
-        className="field-input"
+      <li
+        className="list-group-item"
         style={{
           display: !itemsSchema.custom_int1_name && "none",
         }}
       >
-        <label>{itemsSchema.custom_int1_name}</label>
+        <label className="form-label fw-bold">
+          {itemsSchema.custom_int1_name}
+        </label>
         <input
+          className="form-control"
           onChange={(e) => {
             updateFields("int1", e.target.value);
           }}
           type="number"
           value={itemFieldsValues.custom_int1_value}
         />
-      </div>
+      </li>
 
-      <div
-        className="field-input"
+      <li
+        className="list-group-item"
         style={{
           display: !itemsSchema.custom_int2_name && "none",
         }}
       >
-        <label>{itemsSchema.custom_int2_name}</label>
+        <label className="form-label fw-bold">
+          {itemsSchema.custom_int2_name}
+        </label>
         <input
+          className="form-control"
           onChange={(e) => {
             updateFields("int2", e.target.value);
           }}
           type="number"
           value={itemFieldsValues.custom_int2_value}
         />
-      </div>
+      </li>
 
-      <div
-        className="field-input"
+      <li
+        className="list-group-item"
         style={{
           display: !itemsSchema.custom_int3_name && "none",
         }}
       >
-        <label>{itemsSchema.custom_int3_name}</label>
+        <label className="form-label fw-bold">
+          {itemsSchema.custom_int3_name}
+        </label>
         <input
+          className="form-control"
           onChange={(e) => {
             updateFields("int3", e.target.value);
           }}
           type="number"
           value={itemFieldsValues.custom_int3_value}
         />
-      </div>
+      </li>
 
-      <div
-        className="field-input"
+      <li
+        className="list-group-item"
         style={{
           display: !itemsSchema.custom_bool1_name && "none",
         }}
       >
-        <label>{itemsSchema.custom_bool1_name}</label>
+        <label className="form-label fw-bold">
+          {itemsSchema.custom_bool1_name}
+        </label>
         <input
+          className="form-check mx-auto"
           onChange={(e) => {
             updateFields("bool1", e.target.checked);
           }}
           type="checkbox"
           checked={itemFieldsValues.custom_bool1_value}
         />
-      </div>
+      </li>
 
-      <div
-        className="field-input"
+      <li
+        className="list-group-item"
         style={{
           display: !itemsSchema.custom_bool2_name && "none",
         }}
       >
-        <label>{itemsSchema.custom_bool2_name}</label>
+        <label className="form-label fw-bold">
+          {itemsSchema.custom_bool2_name}
+        </label>
         <input
+          className="form-check mx-auto"
           onChange={(e) => {
             updateFields("bool2", e.target.checked);
           }}
           type="checkbox"
           checked={itemFieldsValues.custom_bool2_value}
         />
-      </div>
+      </li>
 
-      <div
-        className="field-input"
+      <li
+        className="list-group-item"
         style={{
           display: !itemsSchema.custom_bool3_name && "none",
         }}
       >
-        <label>{itemsSchema.custom_bool3_name}</label>
+        <label className="form-label fw-bold">
+          {itemsSchema.custom_bool3_name}
+        </label>
         <input
+          className="form-check mx-auto"
           onChange={(e) => {
             updateFields("bool3", e.target.checked);
           }}
           type="checkbox"
           checked={itemFieldsValues.custom_bool3_value}
         />
-      </div>
+      </li>
 
-      <div
-        className="field-input"
+      <li
+        className="list-group-item"
         style={{
           display: !itemsSchema.custom_date1_name && "none",
         }}
       >
-        <label>{itemsSchema.custom_date1_name}</label>
+        <label className="form-label fw-bold">
+          {itemsSchema.custom_date1_name}
+        </label>
         <input
+          className="form-control"
           onChange={(e) => {
             updateFields("date1", e.target.value);
           }}
           type="date"
           value={itemFieldsValues.custom_date1_value}
         />
-      </div>
+      </li>
 
-      <div
-        className="field-input"
+      <li
+        className="list-group-item"
         style={{
           display: !itemsSchema.custom_date2_name && "none",
         }}
       >
-        <label>{itemsSchema.custom_date2_name}</label>
+        <label className="form-label fw-bold">
+          {itemsSchema.custom_date2_name}
+        </label>
         <input
+          className="form-control"
           onChange={(e) => {
             updateFields("date2", e.target.value);
           }}
           type="date"
           value={itemFieldsValues.custom_date2_value}
         />
-      </div>
+      </li>
 
-      <div
-        className="field-input"
+      <li
+        className="list-group-item"
         style={{
           display: !itemsSchema.custom_date3_name && "none",
         }}
       >
-        <label>{itemsSchema.custom_date3_name}</label>
+        <label className="form-label fw-bold">
+          {itemsSchema.custom_date3_name}
+        </label>
         <input
+          className="form-control"
           onChange={(e) => {
             updateFields("date3", e.target.value);
           }}
           type="date"
           value={itemFieldsValues.custom_date3_value}
         />
-      </div>
+      </li>
 
-      <div
-        className="field-input"
+      <li
+        className="list-group-item"
         style={{
           display: !itemsSchema.custom_multext1_name && "none",
         }}
       >
-        <label>{itemsSchema.custom_multext1_name}</label>
+        <label className="form-label fw-bold">
+          {itemsSchema.custom_multext1_name}
+        </label>
         <textarea
+          className="form-control"
           onChange={(e) => {
             updateFields("multext1", e.target.value);
           }}
           rows="3"
           value={itemFieldsValues.custom_multext1_value}
         ></textarea>
-      </div>
+      </li>
 
-      <div
-        className="field-input"
+      <li
+        className="list-group-item"
         style={{
           display: !itemsSchema.custom_multext2_name && "none",
         }}
       >
-        <label>{itemsSchema.custom_multext2_name}</label>
+        <label className="form-label fw-bold">
+          {itemsSchema.custom_multext2_name}
+        </label>
         <textarea
+          className="form-control"
           onChange={(e) => {
             updateFields("multext2", e.target.value);
           }}
           rows="3"
           value={itemFieldsValues.custom_multext2_value}
         ></textarea>
-      </div>
+      </li>
 
-      <div
-        className="field-input"
+      <li
+        className="list-group-item"
         style={{
           display: !itemsSchema.custom_multext3_name && "none",
         }}
       >
-        <label>{itemsSchema.custom_multext3_name}</label>
+        <label className="form-label fw-bold">
+          {itemsSchema.custom_multext3_name}
+        </label>
         <textarea
+          className="form-control"
           onChange={(e) => {
             updateFields("multext3", e.target.value);
           }}
           rows="3"
           value={itemFieldsValues.custom_multext3_value}
         ></textarea>
-      </div>
+      </li>
     </>
   );
 }
