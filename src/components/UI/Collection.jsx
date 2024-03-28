c1b8e0ca6f4cf19d2f03be3bb75d8f811604206b
MeSailor <themicroline@gmail.com>
2024-03-28 14:03:43 +0400

    buttons moved to another position

--- src/components/UI/Collection.jsx
+++ src/components/UI/Collection.jsx
@@ -10,48 +10,6 @@ export default function Collection({ collection }) {
     <div className="bg-light card mb-3">
       <div className="row">
         <div className="col-md-3 pe-0">
-          {collection.user_id === user.id ? (
-            <>
-              <div
-                style={{ position: "absolute" }}
-                className="btn-group"
-                role="group"
-                aria-label="CollectionManaging"
-              >
-                <Link
-                  className="btn btn-success"
-                  to={`/user/${collection.user_id}/collection/${collection.id}`}
-                >
-                  Open
-                </Link>
-
-                <button
-                  type="button"
-                  className="btn btn-primary"
-                  data-bs-toggle="modal"
-                  data-bs-target={`#CollEditModal${collection.id}`}
-                >
-                  <i className="bi bi-pencil-square"></i>
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-danger"
-                  data-bs-toggle="modal"
-                  data-bs-target={`#CollDeleteModal${collection.id}`}
-                >
-                  <i className="bi bi-trash-fill"></i>
-                </button>
-              </div>
-            </>
-          ) : (
-            <Link
-              style={{ position: "absolute" }}
-              className="btn btn-success"
-              to={`/user/${collection.user_id}/collection/${collection.id}`}
-            >
-              Open
-            </Link>
-          )}
           <img
             src={collection.imageUrl}
             className=" img-fluid rounded-start"
@@ -63,10 +21,48 @@ export default function Collection({ collection }) {
             <div className="row">
               <div className="mt-2 col-md-3 col-12">
                 <h3 className="card-title ">{collection.name}</h3>
-
                 <p className="card-text">
                   <small className="text-muted">{collection.topic}</small>
                 </p>
+                {collection.user_id === user.id ? (
+                  <>
+                    <div
+                      className="btn-group"
+                      role="group"
+                      aria-label="CollectionManaging"
+                    >
+                      <Link
+                        className="btn btn-success"
+                        to={`/user/${collection.user_id}/collection/${collection.id}`}
+                      >
+                        Open
+                      </Link>
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        data-bs-toggle="modal"
+                        data-bs-target={`#CollEditModal${collection.id}`}
+                      >
+                        <i className="bi bi-pencil-square"></i>
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-danger"
+                        data-bs-toggle="modal"
+                        data-bs-target={`#CollDeleteModal${collection.id}`}
+                      >
+                        <i className="bi bi-trash-fill"></i>
+                      </button>
+                    </div>
+                  </>
+                ) : (
+                  <Link
+                    className="btn btn-success"
+                    to={`/user/${collection.user_id}/collection/${collection.id}`}
+                  >
+                    Open
+                  </Link>
+                )}
               </div>
               <div className="mt-2 col-lg-9 col-12">
                 <div
